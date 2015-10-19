adb701291adc14cf3cc0a3920ab7cfc1affbf64f
royriojas <roy@kno.com>
2015-10-19 14:45:19 -0500

    Demo of the bug when trying to use a module that uses a transform as a dependency

--- example/index.js
+++ example/index.js
@@ -3,6 +3,8 @@ var React = require('react');
 import Label from './label.jsx';
 require('./interval')();
 
+require('dtektor/all');
+
 $(document).ready(() => {
   React.render(
     React.createElement(Label, null),
