c2c8d49df9148d31f01d4c32b371411c92824c14
Chris Cowan <agentme49@gmail.com>
2017-11-17 23:40:05 -0800

    Update some dependencies.

--- test/lib/copy.js
+++ test/lib/copy.js
@@ -3,7 +3,7 @@ const fsExtra = require('fs-extra');
 
 export default function copy(src, target) {
   return new Promise((resolve, reject) => {
-    fsExtra.copy(src, target, {clobber:true}, err => {
+    fsExtra.copy(src, target, {overwrite:true}, err => {
       if (err)
         reject((Array.isArray(err) && err[0]) ? err[0] : err);
       else
